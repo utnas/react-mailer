7abd8308b9d71a2ac1ee4030583f10f3abecb390
Dooley <nsewolo@gmail.com>
2014-10-25 21:54:41 -0400

    TODO example added

--- server/mailer/mailer.js
+++ server/mailer/mailer.js
@@ -1,11 +1,42 @@
 (function () {
     'use strict';
 
-    var Hello = React.createClass({
+    /** @jsx React.DOM */
+    var TodoList = React.createClass({displayName: 'TodoList',
         render: function () {
-            return React.DOM.div({}, 'Hello ' + this.props.name);
+            var createItem = function (itemText) {
+                return React.DOM.li(null, itemText);
+            };
+            return React.DOM.ul(null, this.props.items.map(createItem));
+        }
+    });
+    var TodoApp = React.createClass({displayName: 'TodoApp',
+        getInitialState: function () {
+            return {items: [], text: ''};
+        },
+        onChange: function (e) {
+            this.setState({text: e.target.value});
+        },
+        handleSubmit: function (e) {
+            e.preventDefault();
+            var nextItems = this.state.items.concat([this.state.text]);
+            var nextText = '';
+            this.setState({items: nextItems, text: nextText});
+        },
+        render: function () {
+            return (
+                React.DOM.div(null,
+                    React.DOM.h3(null, "TODO"),
+                    TodoList({items: this.state.items}),
+                    React.DOM.form({onSubmit: this.handleSubmit},
+                        React.DOM.input({onChange: this.onChange, value: this.state.text}),
+                        React.DOM.button(null, 'Add #' + (this.state.items.length + 1))
+                    )
+                )
+                );
         }
     });
 
-    React.renderComponent(Hello({name: 'World'}), document.body);
+    React.renderComponent(TodoApp(null), document.body);
+
 })();
