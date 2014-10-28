98c2ce6bf79ece5bf1ef0e40e8224801a8b75467
Dooley <nsewolo@gmail.com>
2014-10-27 21:18:28 -0400

    renaming

--- server/mailer/mailer.js
+++ server/mailer/mailer.js
@@ -1,8 +1,7 @@
 (function () {
     'use strict';
 
-    /** @jsx React.DOM */
-    var TodoList = React.createClass({displayName: 'TodoList',
+    var Mailer = React.createClass({displayName: 'Mailer',
         render: function () {
             var createItem = function (itemText) {
                 return React.DOM.li(null, itemText);
@@ -10,7 +9,8 @@
             return React.DOM.ul(null, this.props.items.map(createItem));
         }
     });
-    var TodoApp = React.createClass({displayName: 'TodoApp',
+
+    var MailerApp = React.createClass({displayName: 'MailerApp',
         getInitialState: function () {
             return {items: [], text: ''};
         },
@@ -26,8 +26,11 @@
         render: function () {
             return (
                 React.DOM.div(null,
-                    React.DOM.h3(null, "TODO"),
-                    TodoList({items: this.state.items}),
+
+                    React.DOM.h3(null, "MAILER"),
+
+                    Mailer({items: this.state.items}),
+
                     React.DOM.form({onSubmit: this.handleSubmit},
                         React.DOM.input({onChange: this.onChange, value: this.state.text}),
                         React.DOM.button(null, 'Add #' + (this.state.items.length + 1))
@@ -37,6 +40,6 @@
         }
     });
 
-    React.renderComponent(TodoApp(null), document.body);
+    React.renderComponent(MailerApp(null), document.body);
 
 })();
